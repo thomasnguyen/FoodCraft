d908a4f9574fe3978bc8cf284bf6a955d7c52249
Thomas Nguyen <imtoshiru@gmail.com>
2018-06-19 21:02:27 -0700

    Added RecipeContainer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import  FilterBar from  './FilterBar';
+import  RecipeContainer from  './RecipeContainer';
 
 class App extends Component {
   render() {
---
+++ src/RecipeContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class RecipeContainer extends Component {
+    render(){
+        return (
+            <div className = "RecipeContainer">
+            </div>
+        );
+    }
+}
+
+export default RecipeContainer;
