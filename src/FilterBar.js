be13002407c4d85ef21d1d5dab87fdfabd18af6a
Thomas Nguyen <imtoshiru@gmail.com>
2018-06-24 19:58:12 -0700

    added search icon into search bar

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -5,7 +5,10 @@ class FilterBar extends Component {
     render(){
         return (
             <div className = "FilterBar">
-                <input  className = "SearchBar" type="text" />
+                <div className ="Search">
+                    <a href="#"><img class="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png" /></a>
+                    <input  className = "SearchBar" type="text" />
+                </div>
             </div>
         );
     }
