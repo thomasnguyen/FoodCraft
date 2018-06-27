6c422e1dc4b956458a0d2bb5c171faade1e143aa
Thomas Nguyen <imtoshiru@gmail.com>
2018-06-26 20:59:56 -0700

    created formatTime helper function

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,8 @@
 let helpers =  {
     formatTime : function(input) {
+        if (input === ""){
+            return "???"
+        }
         var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
           var hours = 0, minutes = 0, seconds = 0, totalseconds;
 
@@ -10,7 +13,7 @@ let helpers =  {
             if (matches[3]) seconds = Number(matches[3]);
             totalseconds = hours * 3600  + minutes * 60 + seconds;
           }
-     return (totalseconds / 60) + ' minutes';
+     return (totalseconds / 60) + ' mins';
     }
   }
   
