f39799992a08f62766aac1ef5371f96ef9ca9d79
Thomas Nguyen <imtoshiru@gmail.com>
2018-06-27 22:50:20 -0700

    added basic modal

--- src/App.js
+++ src/App.js
@@ -4,25 +4,55 @@ import './App.css';
 import  FilterBar from  './FilterBar';
 import  RecipeContainer from  './RecipeContainer';
 import data from './openrecipes1.json';
+
+
+
+
+
 class App extends Component {
   constructor(props){
     super(props); 
       this.state = {
+        showModal: false,
         recipes: [
       ]
     }
   }
   render() {
+    const Modal = ({ handleClose, show, children }) => {
+      const showHideClassName = show ? "modal display-block" : "modal display-none";
+    
+      return (
+        <div className={showHideClassName}>
+          <section className="modal-main">
+            {children}
+            <button onClick={handleClose}>close</button>
+          </section>
+        </div>
+      );
+    };
+
+
+
+
     return (
       <div className="App">
-      <div className="modal_overlay">
-        <div className="modal"></div>
-      </div>
+
+
+
+
+
        <Header />
         <div className = "content">
           <FilterBar />
           <RecipeContainer recipes={data}/>
         </div>
+
+    <Modal show={true} handleClose={this.hideModal} >
+        <p>Modal</p>
+        <p>Data</p>
+      </Modal>
+
       </div>
     );
   }
