449cbb0f60ba605c01e6db6158af8e7f08c4c60d
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-06 08:28:17 -0700

    more style components awesomeness

--- src/FilterIngredients.js
+++ src/FilterIngredients.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import styled from '../node_modules/styled-components';
+
+const Container = styled('div')`
+    width: 100%;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+`;
+
+const Ingredient = styled('div')`
+    padding: 8px;
+    border: 1px solid #e1e4e8;
+    border-bottom: 0px solid white;
+    font-size: 14px;
+    color: #1b1f23;
+
+    &:last-of-type{
+        border: 1px solid #e1e4e8;
+    }
+`;
+
+const Icon = styled('i')`
+    margin-right: 10px;
+    color: gray;
+`;
 
 class FilterIngredients extends Component {
-    render(){
-        let searchResults = this.props.data_ingredient.map((ingredient) => 
-                <div onClick = {() => this.props.addIngredient(ingredient.name)} key = {ingredient.name}> <i className="fas fa-lemon FilteredIngredients_icon"></i>{ingredient.name} </div>
-        );
+	render() {
+		let searchResults = this.props.data_ingredient.map((ingredient) => (
+			<Ingredient onClick={() => this.props.addIngredient(ingredient.name)} key={ingredient.name}>
+				{' '}
+				<Icon className="fas fa-lemon" />
+				{ingredient.name}{' '}
+			</Ingredient>
+		));
 
-        return (
-            <div className = "FilteredIngredients_Box" >
-                {searchResults}
-           </div>
-        );
-    }
+		return <Container>{searchResults}</Container>;
+	}
 }
 
 export default FilterIngredients;
--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import styled from '../node_modules/styled-components';
+
+const Tag = styled('div')`
+
+    border-radius: 10px;
+    /* Chicken: */
+    font-size: 12px;
+    letter-spacing: 0;
+    text-align: left;
+    padding: 10px;
+    font-weight: 500;
+
+    margin-bottom: 10px;
+
+    display: flex;
+    justify-content: space-between;
+
+`;
+
+const Title = styled('div')``;
 
 class Ingredient extends Component {
-    render(){
-        return (
-           <div className = {`ingredient__tag ${this.props.color}`}>
-                <div className = "ingredient__tag__title"> {this.props.name} </div>
-                <div className = "ingredient__tag__title"> <i onClick = {() => this.props.deleteIngredient(this.props.key)}  className="fas fa-times"></i>
-                </div>
-           </div>
-        );
-    }
+	render() {
+		return (
+			<div className={`ingredient__tag ${this.props.color}`}>
+				<Title> {this.props.name} </Title>
+				<Title>
+					{' '}
+					<i onClick={() => this.props.deleteIngredient(this.props.key)} className="fas fa-times" />
+				</Title>
+			</div>
+		);
+	}
 }
 
 export default Ingredient;
