edeb1c44084f84836a1595993e00ed550e84fa8d
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-05 18:52:53 -0700

    configuring styled components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./Header";
 import "./App.css";
 import FilterBar from "./FilterBar";
 import RecipeContainer from "./RecipeContainer";
-import data from "./openrecipes.json";
+import data from "./openrecipes1.json";
 import RecipeModal from "./RecipeModal";
 
 class App extends Component {
--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import helper from './helper';
 
+import styled from 'styled-components';
+
+const Card = styled('article')`
+    width:300px;
+    margin: 10px;
+    transition: all 0.2s ease-out;
+`;
+
+const Title = styled('header')`
+    position:relative;
+    height:225px;
+`;
+
+
+
+
 class RecipeCard extends Component {
     render(){
         return (
-            <div  className = "RecipeCard" onClick = {() => this.props.handleOpen(this.props.title)}>
-                <div className = "RecipeCard__title">
+
+            <Card  onClick = {() => this.props.handleOpen(this.props.title)}>
+                <Title>
                     <div className = "RecipeCard__title__percent">{this.props.percent}</div>
                     <div className = "RecipeCard__title_title">{this.props.title}</div>
                     <img className = "RecipeCard__title_image" src = {this.props.image} alt = ""/>
-                </div>
+                </Title>
+
                 <div className ="RecipeCard__description">
                     <div className = "RecipeCard__info">
                         <div className = "quick_stats">
@@ -34,14 +52,11 @@ class RecipeCard extends Component {
                             <div className="quick_stats__title">Prep Time</div>
                         </div>
                     </div>
-                    { /*
-                    <div className = "RecipeCard__quick-description">{this.props.description.substring(0,85) + '...'}
-                    </div>
-                    */}
                 </div>
-            </div>
+            </Card>
         );
     } 
 }
+
 
 export default RecipeCard;
