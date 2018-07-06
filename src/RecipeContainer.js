28fdfca023bce88cfdb1aec163cddda2a346a110
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-05 23:56:20 -0700

    Integrated percentage randomizer

--- src/RecipeContainer.js
+++ src/RecipeContainer.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import  RecipeCard from  './RecipeCard';
-
+import RecipeCard from './RecipeCard';
+import helpers from './helper';
 
 class RecipeContainer extends Component {
-    render(){
-        let recipeArray = this.props.recipes.map((recipe) =>
-            <RecipeCard title        = {recipe.name}
-                        percent      = "25%"
-                        ingredients  = {recipe.ingredients}
-                        description  = {recipe.description}
-                        image        = {recipe.image} 
-                        cook_time    = {recipe.cookTime}
-                        prep_time    = {recipe.prepTime}
-                        recipe_yield = {recipe.recipeYield}
-                        key          = {recipe.name + recipe.ingredients}
-                        handleOpen   = {this.props.handleOpen} />
-        );
-        return (
-            
-            <div className = "RecipeContainer">
-                {recipeArray}
-            </div>
-        );
-    }
+	render() {
+		let recipeArray = this.props.recipes.map((recipe) => (
+			<RecipeCard
+				title={recipe.name}
+				percent={helpers.randomizer()}
+				ingredients={recipe.ingredients}
+				description={recipe.description}
+				image={recipe.image}
+				cook_time={recipe.cookTime}
+				prep_time={recipe.prepTime}
+				recipe_yield={recipe.recipeYield}
+				key={recipe.name + recipe.ingredients}
+				handleOpen={this.props.handleOpen}
+			/>
+		));
+		return <div className="RecipeContainer">{recipeArray}</div>;
+	}
 }
 
 export default RecipeContainer;
--- src/helper.js
+++ src/helper.js
@@ -1,33 +1,37 @@
-let helpers =  {
-    formatTime : function(input) {
-        if (input === ""){
-            return "???"
-        }
-        var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
-          var hours = 0, minutes = 0, seconds = 0, totalseconds;
+let helpers = {
+	formatTime: function(input) {
+		if (input === '') {
+			return '???';
+		}
+		var reptms = /^PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?$/;
+		var hours = 0,
+			minutes = 0,
+			seconds = 0,
+			totalseconds;
 
-          if (reptms.test(input)) {
-            var matches = reptms.exec(input);
-            if (matches[1]) hours = Number(matches[1]);
-            if (matches[2]) minutes = Number(matches[2]);
-            if (matches[3]) seconds = Number(matches[3]);
-            totalseconds = hours * 3600  + minutes * 60 + seconds;
-          }
-     return (totalseconds / 60) + ' mins';
-    },
-  
-    calcIngredient(ingredients){
-        if (ingredients !== undefined) {
-            return ingredients.split("\n").length;
-        }
-    },
+		if (reptms.test(input)) {
+			var matches = reptms.exec(input);
+			if (matches[1]) hours = Number(matches[1]);
+			if (matches[2]) minutes = Number(matches[2]);
+			if (matches[3]) seconds = Number(matches[3]);
+			totalseconds = hours * 3600 + minutes * 60 + seconds;
+		}
+		return totalseconds / 60 + ' mins';
+	},
 
-  splitIngredient(ingredients){
-      if (ingredients !== undefined)
-        return ingredients.split("\n");
-  }
-  
-  }
+	calcIngredient(ingredients) {
+		if (ingredients !== undefined) {
+			return ingredients.split('\n').length;
+		}
+	},
 
-  
-  export default helpers;
+	splitIngredient(ingredients) {
+		if (ingredients !== undefined) return ingredients.split('\n');
+	},
+
+	randomizer() {
+		return Math.floor(Math.random() * 100 + 1) + '%';
+	}
+};
+
+export default helpers;
