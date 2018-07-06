0c30fb230dff775aa5cc7bc0772f89434c7608eb
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-06 09:42:26 -0700

    Utilized the ultimate power of arrow functions

--- src/Ingredient.js
+++ src/Ingredient.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
 import styled from '../node_modules/styled-components';
 
+const Colors = {
+	blue: {
+		background: '#c0d4ff',
+		color: 'rgb(39, 89, 208)'
+	},
+
+	brown: {
+		background: '#eac097',
+		color: '#843d2f'
+	},
+
+	green: {
+		background: '#bfe5ce',
+		color: '#248441'
+	}
+};
+
 const Tag = styled('div')`
 
     border-radius: 10px;
@@ -16,6 +33,8 @@ const Tag = styled('div')`
     display: flex;
     justify-content: space-between;
 
+    background: ${(props) => Colors[props.color].background};
+    color: ${(props) => Colors[props.color].color};
 `;
 
 const Title = styled('div')``;
@@ -23,13 +42,13 @@ const Title = styled('div')``;
 class Ingredient extends Component {
 	render() {
 		return (
-			<div className={`ingredient__tag ${this.props.color}`}>
+			<Tag color={this.props.color}>
 				<Title> {this.props.name} </Title>
 				<Title>
 					{' '}
 					<i onClick={() => this.props.deleteIngredient(this.props.key)} className="fas fa-times" />
 				</Title>
-			</div>
+			</Tag>
 		);
 	}
 }
