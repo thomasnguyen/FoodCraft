01a4f7ca3cb4004c3ec64830944bdca3127cbd6f
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-08 00:24:19 -0700

    Add button to only the right max-width

--- src/MobileFilter.js
+++ src/MobileFilter.js
@@ -4,7 +4,24 @@ import styled from '../node_modules/styled-components';
 import FilterBar from './FilterBar.js';
 
 const Wrapper = styled('div')`
+	width:100%;
+	height:54px;
+	background:rgba(255,255,255,1);
+	border-bottom: 1px solid rgba(0,0,0,0.1);
+`;
 
+const FilterButton = styled('button')`
+	padding:18px 25px;
+	font-size:15px;
+	border:0px solid;
+	background:rgba(0,0,0,0.05);
+	height:100;
+	float:right;
+	outline:none;
+	&:hover{
+		cursor:pointer;
+		background:rgba(0,0,0,.1)
+	}
 
 `;
 
@@ -12,8 +29,10 @@ class MobileFilter extends Component {
 	render() {
 		return (
 			<Wrapper>
-				<button>Filters</button>
-				<FilterBar />
+				<FilterButton>
+					{' '}
+					<i class="fas fa-filter" /> Apply Filters
+				</FilterButton>
 			</Wrapper>
 		);
 	}
