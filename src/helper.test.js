5ced627f10c1a9e0c06639623bd65ca2bc996f1e
Thomas Nguyen <imtoshiru@gmail.com>
2018-07-27 00:39:23 -0700

    Example of a fail

--- src/helper.test.js
+++ src/helper.test.js
@@ -1,5 +1,10 @@
 import helper from './helper';
 
+// Sluggify Function
 test('Sluggify: Thomas Nguyen -> thomas-nguyen', () => {
 	expect(helper.sluggify('Thomas Nguyen')).toBe('thomas-nguyen');
+});
+
+test('Sluggify: Two spaces', () => {
+	expect(helper.sluggify('Pepporoni  Pizza')).toBe('pepporoni-pizza');
 });
